586136f2e65c73c1f5d4ba5722ac03f637e9f582
Sharat Acharjee mugdho <Sharatacharjee6@gmail.com>
2025-09-23 11:36:58 +0600

    Update settings.py

--- chaiheadq/settings.py
+++ chaiheadq/settings.py
@@ -29,7 +29,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
+  STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
